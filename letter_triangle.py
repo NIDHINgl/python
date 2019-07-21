c060b072a6b643a8772ba7ff0f3ab3579a591755
NIDHINgl <38777673+NIDHINgl@users.noreply.github.com>
2019-07-21 16:23:37 +0530

    Add files via upload

---
+++ letter_triangle.py
@@ -0,0 +1,10 @@
+def triangle(n,num):
+    for i in range(0,n):
+        for j in range(0,i+1):
+            letter = chr(num)
+            print(letter,end=" ")
+            num = num + 1
+        print('\r')
+n = 5
+num = 65
+triangle(5,65)
