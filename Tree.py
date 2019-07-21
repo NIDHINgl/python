f0d895ab8ca61e28731e8b79a2298191bdfaa782
NIDHINgl <38777673+NIDHINgl@users.noreply.github.com>
2019-07-21 13:31:07 +0530

    Tree.py
    python code to draw a tree

---
+++ Tree.py
@@ -0,0 +1,20 @@
+def leaf(n):
+	k = 2 * n - 2
+	for i in range(0,n):
+		for j in range(0,k):
+			print(end=" ")
+		k = k - 1
+		for j in range(0,i+1):
+			print("* ",end="")
+		print("\r")
+	stem(n)
+def stem(n):
+	x = 2 * n - 3
+	for i in range(0,2):
+		for j in range(0,x):
+			print(end=" ")
+		print("| |",end="")
+		print("\r")
+
+n = int(input())
+leaf(n)
