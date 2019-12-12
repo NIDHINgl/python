9725cb11524386618da15ebf12c9a59124669710
NIDHINgL <38777673+NIDHINgl@users.noreply.github.com>
2019-12-12 12:14:05 +0530

    Update mark.py

--- mark.py
+++ mark.py
@@ -1,4 +1,4 @@
-"""Given the names and grades for each student in a Physics class of students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+#Given the names and grades for each student in a Physics class of students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
 
 **Note**: If there are multiple students with the same grade, order their names alphabetically and print each name on a new line.
 
@@ -6,7 +6,7 @@
     The first line contains an integer,N , the number of students.
     The 2N subsequent lines describe each student over lines; the first line contains a student's name, and the second line contains their grade. 
 **Output Format**
-     Print the name(s) of any student(s) having the second lowest grade in Physics; if there are multiple students, order their names alphabetically and print each one on a new line."""
+     Print the name(s) of any student(s) having the second lowest grade in Physics; if there are multiple students, order their names alphabetically and print each one on a new line.
 
 #hacker rank rested list question
 index  = []
